fd0b8112ea66f240aa0d2ea7b4a26da14ba041fd
luochuangjie <chuangjie.luo@vivo.com>
2021-07-02 19:13:06 +0800

    add env support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()]
-})
+export default ({ command, mode }) => {
+  const configBase = {
+    plugins: [
+      reactRefresh(),
+    ],
+    envDir: 'envs',
+    build: {
+      outDir: '.dist'
+    }
+  }
+  if (command === 'build') {
+    configBase.plugins.push(legacy({
+      targets: ['defaults'],
+      // additionalLegacyPolyfills: ['regenerator-runtime/runtime'] 
+    }))
+    Object.assign(configBase, {
+      // base: '//cdn.baidu.com/assets/',
+      base: '//localhost:3001/',
+    })
+  }
+  return defineConfig(configBase)
+}
